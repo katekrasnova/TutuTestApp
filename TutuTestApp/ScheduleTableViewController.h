38888601dc55ab30a9e2efcbcce8f0cb92e1111f
katekrasnova <katekrasnova1990@gmail.com>
2016-12-08 23:12:35 +0300

    ScheduleTableViewController class added

---
+++ TutuTestApp/ScheduleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScheduleTableViewController.h
+//  TutuTestApp
+//
+//  Created by Ekaterina Krasnova on 08/12/2016.
+//  Copyright © 2016 Ekaterina Krasnova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScheduleTableViewController : UITableViewController
+
+@end
